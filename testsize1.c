b407ad8ee48c874bf798dd34d7fdd75f0f6b1c4c
xiongyy2 <x_gandalf@163.com>
2017-05-07 18:46:53 -0400

    try use long_new_array() and float_new_array()

--- testsize1.c
+++ testsize1.c
@@ -3,6 +3,41 @@
 #include<stdbool.h>
 #include<math.h>
 
+void* long_new_array(const size_t N,const char* error_msg)
+{
+	void* ptr;
+	int err;
+	/*if (_deviceType == 3){
+		err = posix_memalign(&ptr,ACL_ALIGNMENT,N * sizeof(int));
+		check(err == 0,error_msg);
+	}
+	else{
+		ptr = malloc(N * sizeof(int));
+		check(ptr != NULL,error_msg);
+	}*/
+	ptr = malloc(N * sizeof(long));
+	check(ptr != NULL,error_msg);
+	return ptr;
+}
+
+void* float_new_array(const size_t N,const char* error_msg)
+{
+	void* ptr;
+	int err;
+	/*if (_deviceType == 3){
+		err = posix_memalign(&ptr,ACL_ALIGNMENT,N * sizeof(float));
+		check(!err,error_msg);
+	}
+	else{
+		ptr = malloc(N * sizeof(float));
+		check(ptr != NULL,error_msg);
+	}*/
+	ptr = malloc(N * sizeof(float));
+	check(ptr != NULL,error_msg);
+	return ptr;
+}
+
+
 int main(int argc, char *argv[])
 {
     int num_row=2048;
@@ -11,11 +46,14 @@ int main(int argc, char *argv[])
     unsigned long *row_ptr;
     unsigned long *col_idx;
     float *val;
-    row_ptr=malloc((num_row+1) * sizeof(unsigned long));
+    //row_ptr=malloc((num_row+1) * sizeof(unsigned long));
+    row_ptr=long_new_array(num_row+1,"sparse_formats.read_csr() - Heap Overflow! Cannot allocate space for row_ptr");
     printf("row_ptr alloced %d\n",num_row+1);
-    col_idx=malloc(num_nonzero * sizeof(unsigned long));
+    //col_idx=malloc(num_nonzero * sizeof(unsigned long));
+    col_idx=long_new_array(num_nonzero,"sparse_formats.read_csr() - Heap Overflow! Cannot allocate space for col_idx");
     printf("col_idx alloced %ld\n",num_nonzero);
-    val=malloc(num_nonzero * sizeof(float));
+    //val=malloc(num_nonzero * sizeof(float));
+    val=float_new_array(num_nonzero,"sparse_formats.read_csr() - Heap Overflow! Cannot allocate space for val");
     printf("val alloced %ld\n",num_nonzero);
 
     free(row_ptr);
