329fa1690e025fd261d00b6a46abcbfe1583bdea
xiongyy2 <x_gandalf@163.com>
2017-05-08 14:16:02 -0400

    added testcsr5.c to test something

---
+++ rewrite1/testcsr5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<math.h>
+
+int main(int argc, char *argv[])
+{
+    unsigned int tile_ptr;
+    tile_ptr=-0;
+    printf("tile_ptr=%u\n",tile_ptr);
+}
