596630c427cb45f91754022ea87db464385b15c0
xiongyy2 <x_gandalf@163.com>
2017-05-10 00:08:34 -0400

    added free() to csr5 omp

--- rewrite1/csr5_ompacc.c
+++ rewrite1/csr5_ompacc.c
@@ -222,6 +222,7 @@ void spmv_csr_acc(const unsigned long num_rows,const unsigned long num_cols,cons
                 y_offset[i6]=empty_offset[y_offset[i6]];
             }
             printf("empty_offset generated\n");
+            free(empty_offset);
         }
         
         float* tmp;
@@ -321,6 +322,9 @@ void spmv_csr_acc(const unsigned long num_rows,const unsigned long num_cols,cons
         }
         free(tmp);
         free(last_tmp);
+        free(y_offset);
+        free(seg_offset);
+        free(tmp_bit);
     }
 }
     if(p>p_cmplt)
@@ -342,4 +346,7 @@ void spmv_csr_acc(const unsigned long num_rows,const unsigned long num_cols,cons
             out[row] = sum;
         }
     }
+    free(tile_ptr);
+    free(tile_ptr_empty);
+    free(bit_flag);
 }
