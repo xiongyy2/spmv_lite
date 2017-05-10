a971c2858c6d4bcaa4e2aa2c720d5ff9c7e33d97
xiongyy2 <x_gandalf@163.com>
2017-05-10 03:05:24 -0400

    update csr acc

--- rewrite1/csr_ompacc.c
+++ rewrite1/csr_ompacc.c
@@ -2,14 +2,14 @@
 
 void spmv_csr_acc(const unsigned long num_rows,const unsigned long num_cols,const unsigned long num_nonzeros,const unsigned long* row_ptr,const unsigned long* col_idx,const float* val,const float* x,const float* y,float* out)
 {
-#pragma acc data copy(num_rows,num_cols,num_nonzeros,row_ptr[0:num_rows],col_idx[0:num_nonzeros-1],val[0:num_nonzeros-1],x[0:num_cols-1],y[0:num_rows-1],out[0:num_rows-1])
+#pragma acc data copy(row_ptr[0:num_rows],col_idx[0:num_nonzeros-1],val[0:num_nonzeros-1],x[0:num_cols-1],y[0:num_rows-1],out[0:num_rows-1])
 {
     unsigned long row,row_start,row_end,jj;
 	float sum = 0;
 #pragma omp parallel 
 	{
 	#pragma omp for private(row, row_start, row_end, jj, sum)
-#pragma acc kernels loop private(row, row_start, row_end, jj, sum)
+#pragma acc kernels loop 
 		for(row=0; row < num_rows; row++)
 		{
 			sum = y[row];
